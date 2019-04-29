c1a91c34a4a1dca77d877b6a3ab717df8f3a8c03
Andrzej Sikorski <andrzej.zbigniew.sikorski@gmail.com>
2019-04-29 18:37:11 +0200

    Finish styling menu header, add some initial click handling

--- src/Menu.js
+++ src/Menu.js
@@ -3,11 +3,14 @@ import styled from 'styled-components';
 
 import UserInfo from './UserInfo';
 
+import logo from './awaymoFullWhite.svg';
+
 const mainColor = '#EE5F63';
 const textColor = '#FFF';
+const borderColor = '#F18990';
 
 const StyledMenu = styled.div`
-  display: grid;
+  display: ${props => (props.visible) ? `grid` : `none`  };
   grid-template-rows: auto auto auto auto;
   grid-template-columns: auto;
   grid-template-areas: 
@@ -18,11 +21,48 @@ const StyledMenu = styled.div`
   
   color: ${textColor};
   background: ${mainColor};
+  
+  @media (min-width: 700px) {}
+`
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  
+  padding: 20px 0;
+  margin: 0 15px;
+  
+  border-bottom: 2px solid ${borderColor};
+  
+  i {
+    cursor: pointer;
+    font-size: 1.15rem;
+  }
+  
+  @media (min-width: 700px) {
+    border-bottom: 3px solid #FFF;
+  }
 `
 
 class MenuHeader extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick() {
+    this.props.toggleVisibility();
+  }
+
   render() {
-    return <div>Header</div>
+    return (
+      <Header>
+        <div></div>
+        <img src={logo} alt="logo"/>
+        <i onClick={this.handleClick} className="fas fa-times"></i>
+      </Header>
+    )
   }
 }
 
@@ -45,14 +85,30 @@ class MenuList extends Component {
 }
 
 class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: true
+    }
+    
+    this.toggleVisibility = this.toggleVisibility.bind(this);
+  }
+  
+  toggleVisibility() {
+    this.setState({visible: !this.state.visible})
+  }
+
   render() {
     return (
-      <StyledMenu>
-        <MenuHeader />
-        <UserInfo />
-        <MenuList />
-        <MenuFooter />
-      </StyledMenu>
+      <div>
+        <div onClick={this.toggleVisibility}>Menu</div>
+        <StyledMenu visible={this.state.visible}>
+          <MenuHeader toggleVisibility={this.toggleVisibility}/>
+          <UserInfo />
+          <MenuList />
+          <MenuFooter />
+        </StyledMenu>
+      </div>
     )
   }
 }
